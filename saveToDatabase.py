d0d087aae954024cc3b9feb41926168a1b8646ce
Ben McKinley <bmckinley@fanatics.com>
2019-01-06 19:17:32 -0800

    database interactions working ok

--- saveToDatabase.py
+++ saveToDatabase.py
@@ -15,16 +15,23 @@ def getCursor(conn)	:
 	c = conn.cursor()
 	return c
 
-def dropTables(c):
+def dropTables(conn):
+	c = conn.cursor()
 	print('dropping table')
 	c.execute(''' DROP TABLE IF EXISTS songs''')
 	c.execute(''' DROP TABLE IF EXISTS charts ''')
 	sys.stdout.flush()
 
-def createTables(c):
+def connectAndCreate():
+	conn = connect()
+	createTables(conn)
+	return conn
+
+def createTables(conn):
+	c = conn.cursor()
 	print("creating table")
 	c.execute(''' CREATE TABLE IF NOT EXISTS songs
-					(id not null integer primary key, place integer, name text, artist text, dateString not null text) ''')
+					(id integer primary key, place integer, name text, artist text, dateString text) ''')
 	c.execute(''' CREATE TABLE IF NOT EXISTS charts 
 					(id integer primary key, type text, dateString text unique) ''')
 	sys.stdout.flush()
@@ -46,14 +53,23 @@ def saveChart(chart, conn):
 	sys.stdout.flush()
 	print('saving chart')
 	c = conn.cursor()
-	c.execute(''' INSERT INTO charts VALUES (?, ?) ''', ("hot-100", chart.date))
+	countTuple = c.execute(''' SELECT count(*) from charts WHERE dateString IS ? ''', (chart.date,)).fetchone()
+	count = countTuple[0]
+	if count > 0:
+		print("count > 0")
+		return
+	try:
+		c.execute(''' INSERT INTO charts(type, dateString) VALUES (?, ?) ''', ("hot-100", chart.date))
+	except sqlite3.IntegrityError:
+		print("chart exists")
+		return
 	for i, song in enumerate(chart.entries):
-		c.execute(''' INSERT INTO songs VALUES (?, ?, ?, ?) ''', (i, song.title, song.artist, chart.date))
+		c.execute(''' INSERT INTO songs(place, name, artist, dateString) VALUES (?, ?, ?, ?) ''', (i, song.title, song.artist, chart.date))
 	print('getting new chart')
 	sys.stdout.flush()
 	conn.commit()
 
-def scrapeDataForYear(year):
+def scrapeDataForYear(year, conn):
 	finalDate = getFinalDate(year)
 	lastChart = billboard.ChartData('hot-100', date=finalDate, fetch=True, timeout=30)
 	prevYear = getPreviousYear(year)
