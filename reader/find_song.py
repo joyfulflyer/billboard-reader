eddf8d96d928f7f2af76ae50efb9e48abf14192e
Ben McKinley <bmckinley@fanatics.com>
2019-01-13 19:48:03 -0800

    search and get song out

--- reader/find_song.py
+++ reader/find_song.py
@@ -17,11 +17,37 @@ def search():
 
 @bp.route('/search_results', methods=("GET",))
 def search_results():
-    return render_template('search_results.html')
+    name = request.args['name']
+    name = convertToSpaces(name)
+    artist = request.args['artist']
+    artist = convertToSpaces(artist)
+
+    songs = get_db().execute('''
+            SELECT * FROM songs WHERE name=? AND artist=?
+                             ''', (name, artist)).fetchall()
+    dicts = []
+    for s in songs:
+        dicts.append({
+                     "position": s[0],
+                     "name": s[1],
+                     "artist": s[2],
+                     "date": s[3]
+                     })
+
+    return json.dumps(dicts)
 
 
 @bp.route('/partialSong/<input>', methods=("GET",))
 def partial_song(input):
-    print("Getting " + input)
-    songs = get_db().execute('SELECT DISTINCT name, artist FROM songs WHERE UPPER(name) LIKE UPPER(?)', ('%'+input+'%',)).fetchall()
+    print("raw input " + input)
+    converted = convertToSpaces(input)
+    print("converted = " + converted)
+    songs = get_db().execute('''
+            SELECT DISTINCT name, artist FROM songs
+            WHERE UPPER(name) LIKE UPPER(?)
+            ''', ('%' + converted + '%',)).fetchall()
     return str(songs)
+
+
+def convertToSpaces(input):
+    return " ".join(input.split('_'))
