1bdb250517413fab034920f8568df992c616f7d0
Ben McKinley <bmckinley@fanatics.com>
2019-03-14 17:31:27 -0700

    load songs for year

--- grabber/load_songs.py
+++ grabber/load_songs.py
@@ -12,7 +12,6 @@ def scrape_for_year(year):
     firstChart = billboard.ChartData('hot-100', date=yearString + '-01-01',
                                      fetch=True)
     chart = firstChart
-    print(Session)
     while yearString in chart.date:
         print("Saving date" + chart.date)
         saver.save_chart(chart, Session)
